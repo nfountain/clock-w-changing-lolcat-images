566581d29008bc84a816dc2742d744f5d4ec72e5
Nicena Fontaine <nicenas.email@gmail.com>
2019-01-09 20:53:12 -0500

    Update lolcat images to change at set times with message changes.

--- js/script.js
+++ js/script.js
@@ -11,18 +11,25 @@ var lunchTime = 12;
 var wakeupTime = 7;
 var message;
 var messageOfHour = document.getElementById("timeEvent");
+var lolcatImage = document.getElementById("lolcat");
+
+// ADDED var image - HAVE SAVED, BUT HAVE NOT COMMITTED, as I still need to get the new images to render properly - I think I broke my if-then-else statements, below.
 
 // Message
 if (currentTime == partyTime) {
+    lolcatImage.src = "https://s3.amazonaws.com/media.skillcrush.com/skillcrush/wp-content/uploads/2016/09/cat4.jpg";
 	var message = "Let's party!";
 }
 else if (currentTime == napTime) {
+    lolcatImage.src = "https://s3.amazonaws.com/media.skillcrush.com/skillcrush/wp-content/uploads/2016/09/cat3.jpg";
 	var message = "Time for a nap.";
 }
 else if (currentTime == lunchTime) {
+    lolcatImage.src = "https://s3.amazonaws.com/media.skillcrush.com/skillcrush/wp-content/uploads/2016/09/cat2.jpg";
 	var message = "Time for lunch!";
 }
 else if (currentTime == wakeupTime) {
+    lolcatImage.src = "https://s3.amazonaws.com/media.skillcrush.com/skillcrush/wp-content/uploads/2016/09/cat1.jpg";
 	var message = "Good morning! Time to get up.";
 }
 else if (currentTime < noon) {
@@ -33,7 +40,7 @@ else if (currentTime > evening) {
 }
 else {
 	var message = "Good afternoon!";
-} 
+}
 
 // Dynamically puts the message where the timeEvent ID lives
 messageOfHour.innerText = message;
