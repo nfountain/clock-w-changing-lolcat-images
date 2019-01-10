33fa6c1d51695d0b4aedbbc9d131b86c73cb0dae
Nicena Fontaine <nicenas.email@gmail.com>
2019-01-10 09:42:14 -0500

    Add clock and set to increment one second at a time.

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
 // Test link is working correctly
 console.log("Hello World");
 
-// Variables
-var currentTime = new Date().getHours();
+// Global Variables
+var currentHour = new Date().getHours();
 var noon = 12;
 var evening = 17;
 var partyTime = 21;
@@ -10,31 +10,65 @@ var napTime = 14;
 var lunchTime = 12;
 var wakeupTime = 7;
 var message;
-var messageOfHour = document.getElementById("timeEvent");
-var lolcatImage = document.getElementById("lolcat");
+var messageOfHour = document.getElementById('timeEvent');
+var lolcatImage = document.getElementById('lolcat');
 var image = "https://s3.amazonaws.com/media.skillcrush.com/skillcrush/wp-content/uploads/2016/08/normalTime.jpg";
 
+// Clock
+
+var updateClock = function() {
+	var showCurrentTime = function() {
+		var clock = document.getElementById('clock')
+		var currentTime = new Date();
+		var hours = currentTime.getHours();
+		var minutes = currentTime.getMinutes();
+		var seconds = currentTime.getSeconds();
+		var meridian = "AM";
+
+		if(hours >= noon) {
+			meridian = "PM";
+		}
+		if(hours > noon) {
+			hours = (hours - 12);
+		}
+
+		if (minutes < 10) {
+			minutes = "0" + minutes;
+		}
+
+		if (seconds < 10) {
+			seconds = "0" + seconds;
+		}
+
+		var clockTime = hours + ":" + minutes + ":" + seconds + " " + meridian + "!";
+
+		clock.innerText = clockTime;
+	};
+
+	showCurrentTime();
+}
+
 // Message
-if (currentTime == partyTime) {
+if (currentHour == partyTime) {
     image = "https://s3.amazonaws.com/media.skillcrush.com/skillcrush/wp-content/uploads/2016/09/cat4.jpg";
 	var message = "Let's party!";
 }
-else if (currentTime == napTime) {
+else if (currentHour == napTime) {
     image = "https://s3.amazonaws.com/media.skillcrush.com/skillcrush/wp-content/uploads/2016/09/cat3.jpg";
 	var message = "Time for a nap.";
 }
-else if (currentTime == lunchTime) {
+else if (currentHour == lunchTime) {
     image = "https://s3.amazonaws.com/media.skillcrush.com/skillcrush/wp-content/uploads/2016/09/cat2.jpg";
 	var message = "Time for lunch!";
 }
-else if (currentTime == wakeupTime) {
+else if (currentHour == wakeupTime) {
     image = "https://s3.amazonaws.com/media.skillcrush.com/skillcrush/wp-content/uploads/2016/09/cat1.jpg";
 	var message = "Good morning! Time to get up.";
 }
-else if (currentTime < noon) {
+else if (currentHour < noon) {
 	var message = "Good morning!";
 }
-else if (currentTime > evening) {
+else if (currentHour > evening) {
 	var message = "Good evening!";
 }
 else {
@@ -44,3 +78,7 @@ else {
 // Dynamically puts the message where the timeEvent ID lives
 messageOfHour.innerText = message;
 lolcatImage.src = image;
+
+updateClock();
+var oneSecond = 1000;
+setInterval(updateClock, oneSecond);
