6c9cc20ddaf5679f7646d3c2f9e297ea155c041c
Nicena Fontaine <nicenas.email@gmail.com>
2019-01-09 19:57:52 -0500

    Add messages based on hour of day from previous coursework.

--- js/script.js
+++ js/script.js
@@ -1,1 +1,37 @@
+// Test link is working correctly
 console.log("Hello World");
+
+// Variables
+var currentTime = new Date().getHours();
+var noon = 12;
+var evening = 17;
+var partyTime = 21;
+var napTime = 14;
+var lunchTime = 12;
+var wakeupTime = 7;
+var message;
+
+// Message
+if (currentTime == partyTime) {
+	var message = "Let's party!";
+}
+else if (currentTime == napTime) {
+	var message = "Time for a nap.";
+}
+else if (currentTime == lunchTime) {
+	var message = "Time for lunch!";
+}
+else if (currentTime == wakeupTime) {
+	var message = "Good morning! Time to get up.";
+}
+else if (currentTime < noon) {
+	var message = "Good morning!";
+}
+else if (currentTime > evening) {
+	var message = "Good evening!";
+}
+else {
+	var message = "Good afternoon!";
+} 
+
+console.log(message);
